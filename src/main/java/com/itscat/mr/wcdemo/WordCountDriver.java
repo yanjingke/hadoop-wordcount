3379932ed7610675dee5c07bd466cbb4f0a3fa19
59249 <592499985@qq.com>
2018-06-26 09:58:11 +0800

    Initial commit

---
+++ src/main/java/com/itscat/mr/wcdemo/WordCountDriver.java
@@ -0,0 +1,37 @@
+package com.itscat.mr.wcdemo;
+
+
+import org.apache.hadoop.conf.Configuration;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class WordCountDriver {
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf);
+        job.setJarByClass(WordCountDriver.class);
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountReducer.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        //指定job输入原始文件所在的目录
+        FileInputFormat.setInputPaths(job,new Path(args[0]));
+        //指定job输出原始文件所在的目录
+        FileOutputFormat.setOutputPath(job,new Path(args[1]));
+        boolean res = job.waitForCompletion(true);
+        System.exit(res?0:1);
+        //com.itscat.mr.wcdemo.WordCountDriver.java
+    }
+}
